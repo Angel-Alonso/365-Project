11dbdafc274295823ddc2dfdacb1c6577a4d927b
jonathan fitzgerald <jfitzg09@calpoly.edu>
2026-05-05 01:29:42 -0700

    alembic changes

--- alembic/versions/a4a6e9d8af8d_first_version_to_create_new_table_schema.py
+++ alembic/versions/a4a6e9d8af8d_first_version_to_create_new_table_schema.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a4a6e9d8af8d'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = 'e91d0c24f7d0'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
